82b69a8579f86b437ec869547881502286881a2d
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-04-12 15:17:13 +0000

    Done.

--- lib/hello_ruby_programmer.rb
+++ lib/hello_ruby_programmer.rb
@@ -1,6 +1,4 @@
 def greeting(name)
   puts "Hi, #{name}! Welcome to the wonderful world of Ruby programming."
 end
-puts "Please enter your name so that we can greet you more personally:"
-name=gets.strip
-greeting(name)
+
