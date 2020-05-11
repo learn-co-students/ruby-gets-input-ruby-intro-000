34b84a7d42320052bfb142c8576521181f5c4f9d
Ermite Rikudo <opouth@hotmail.fr>
2020-05-11 18:58:45 +0000

    Done.

--- lib/hello_ruby_programmer.rb
+++ lib/hello_ruby_programmer.rb
@@ -1,3 +1,7 @@
-def greeting
+puts "Please enter your name so that we can greet you more personally:"
+name = gets.strip
+
+def greeting(name="Stranger")
   puts "Hi, #{name}! Welcome to the wonderful world of Ruby programming."
 end
+greeting(name)
